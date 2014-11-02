a408c68cb22ebcd4804b189eddaeca687fa16412
Jon Moss <me@jonathanmoss.me>
2014-11-02 17:27:25 -0500

    Migrated devise user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102205046) do
+ActiveRecord::Schema.define(version: 20141102222453) do
 
   create_table "applications", force: true do |t|
     t.string   "name"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20141102205046) do
     t.text     "about"
     t.string   "tagline"
     t.string   "location"
-    t.string   "slug", :unique => true
+    t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "start"
@@ -66,5 +66,23 @@ ActiveRecord::Schema.define(version: 20141102205046) do
     t.integer  "resume_file_size"
     t.datetime "resume_updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
