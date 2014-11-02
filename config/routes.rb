a6e9c49ec69df721f72eda332c063fe12d78973d
Jon Moss <me@jonathanmoss.me>
2014-11-02 15:04:29 -0500

    App routes are good

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :applications
-
   # Root
   root to: 'pages#home'
 
@@ -11,6 +9,7 @@ Rails.application.routes.draw do
 
   # Scaffolds
   resources :hackathons
+  resources :applications
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
