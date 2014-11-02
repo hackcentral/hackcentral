079e9ae8b564ad004b3a399a2d0429cd24187399
Jon Moss <me@jonathanmoss.me>
2014-11-02 17:25:40 -0500

    devise routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   # Root
   root to: 'pages#home'
 
@@ -11,6 +10,12 @@ Rails.application.routes.draw do
   # Scaffolds
   resources :hackathons
   resources :profiles
+
+  # Devise
+  devise_for :users
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
