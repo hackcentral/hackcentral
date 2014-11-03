f6c34996dc9f951cfcaf39ae96fea944fc942951
Jon Moss <me@jonathanmoss.me>
2014-11-02 20:03:54 -0500

    controller work for applications

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,42 +1,76 @@
 class ApplicationsController < ApplicationController
   before_action :set_application, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
+  # GET /applications
+  # GET /applications.json
   def index
-    @applications = Application.all
-    respond_with(@applications)
+    @applications = Application.where(user_id: current_user.id).all
   end
 
+  # GET /applications/1
+  # GET /applications/1.json
   def show
-    respond_with(@application)
   end
 
+  # GET /applications/new
   def new
-    @application = Application.new
-    respond_with(@application)
+    @application = current_user.applications.build #Application.new
   end
 
+  # GET /applications/1/edit
   def edit
   end
 
+  # POST /applications
+  # POST /applications.json
   def create
-    @application = Application.new(application_params)
-    @application.save
-    respond_with(@application)
+    @application = current_user.applications.build(application_params) #Application.new(application_params)
+
+    respond_to do |format|
+      if @profile.save
+        format.html { redirect_to @application, notice: 'Application was successfully created.' }
+        format.json { render :show, status: :created, location: @application }
+      else
+        format.html { render :new }
+        format.json { render json: @application.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /applications/1
+  # PATCH/PUT /applications/1.json
   def update
-    @application.update(application_params)
-    respond_with(@application)
+    respond_to do |format|
+      if @application.update(application)
+        format.html { redirect_to @application, notice: 'Application was successfully updated.' }
+        format.json { render :show, status: :ok, location: @application }
+      else
+        format.html { render :edit }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /application/1
+  # DELETE /application/1.json
   def destroy
     @application.destroy
-    respond_with(@application)
+    respond_to do |format|
+      format.html { redirect_to applications_url, notice: 'Application was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
     def set_application
       @application = Application.find(params[:id])
+    end
+
+    def correct_user
+      @application = current_user.applications.find_by(id: params[:id])
+      redirect_to applications_path, notice: "Not authorized to edit this application" if @application.nil?
     end
 
     def application_params
