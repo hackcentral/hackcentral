ecd99b9b5e3e0adad77b9186c8f808f6a95a1a05
Jon Moss <me@jonathanmoss.me>
2014-11-02 19:50:11 -0500

    Profile model validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,11 @@ class Profile < ActiveRecord::Base
   validates_attachment_content_type :resume, :content_type => "application/pdf"
 
   belongs_to :user
+
+  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :school_grad, presence: true
+  validates :bio, presence: true
 end
