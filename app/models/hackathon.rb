6a0180cae5b2e1036f9f4f1a424f72bea630eb88
Jon Moss <me@jonathanmoss.me>
2014-11-02 19:51:49 -0500

    Hackathon model validations

--- app/models/hackathon.rb
+++ app/models/hackathon.rb
@@ -4,4 +4,12 @@ class Hackathon < ActiveRecord::Base
 
   has_attached_file :header, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :header, :content_type => /\Aimage\/.*\Z/
+
+  validates :name, presence: true
+  validates :about, presence: true
+  validates :tagline, presence: true
+  validates :location, presence: true
+  validates :slug, presence: true
+  validates :start, presence: true
+  validates :end, presence: true
 end
