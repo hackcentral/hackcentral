13ea3f98c16bfaf2f32caa54e6513e0e61a9a04c
Jon Moss <me@jonathanmoss.me>
2014-11-03 20:37:47 -0500

    submission controller work

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,45 +1,79 @@
 class SubmissionsController < ApplicationController
+  before_action :set_hackathon
   before_action :set_submission, only: [:show, :edit, :update, :destroy]
 
+  # GET /submissions
+  # GET /submissions.json
   def index
-    @submissions = Submission.all
-    respond_with(@submissions)
+    @submissions = @hackathon.submissions #Submission.all
   end
 
+  # GET /submissions/1
+  # GET /submissions/1.json
   def show
-    respond_with(@submission)
   end
 
+  # GET /submissions/new
   def new
-    @submission = Submission.new
-    respond_with(@submission)
+    @submission = current_user.submissions.build #Submission.new
   end
 
+  # GET /submissions/1/edit
   def edit
   end
 
+  # POST /submissions
+  # POST /submissions.json
   def create
-    @submission = Submission.new(submission_params)
-    @submission.save
-    respond_with(@submission)
+    @submission = @hackathon.submissions.build(submission_params) #Submission.new(submission_params)
+
+    respond_to do |format|
+      if @submission.save
+        format.html { redirect_to hackathon_submissions_path, notice: 'Submission was successfully created.' }
+        format.json { render :show, status: :created, location: @submission }
+      else
+        format.html { render :new }
+        format.json { render json: @submission.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /submissions/1
+  # PATCH/PUT /submissions/1.json
   def update
-    @submission.update(submission_params)
-    respond_with(@submission)
+    respond_to do |format|
+      if @submission.update(submission_params)
+        format.html { redirect_to hackathon_submission_path, notice: 'Submission was successfully updated.' }
+        format.json { render :show, status: :ok, location: @submission }
+      else
+        format.html { render :edit }
+        format.json { render json: @submission.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /submissions/1
+  # DELETE /submissions/1.json
   def destroy
     @submission.destroy
-    respond_with(@submission)
+    respond_to do |format|
+      format.html { redirect_to hackathon_submissions_path, notice: 'Submission was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_submission
-      @submission = Submission.find(params[:id])
+      @submission = @hackathon.submissions.find(params[:id])
     end
 
+    def set_hackathon
+      @hackathon = Hackathon.find(params[:hackathon_id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
     def submission_params
-      params.require(:submission).permit(:title, :tagline, :description, :video, :website)
+      params.require(:submission).permit(:title, :tagline, :description, :video, :website, :user_id, :hackathon_id)
     end
 end
