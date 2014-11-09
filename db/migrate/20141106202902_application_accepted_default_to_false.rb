70573d2fec6fe93fc816192971d91a80f808240a
Jon Moss <me@jonathanmoss.me>
2014-11-09 14:21:27 -0500

    Removed duplicate DB migration

--- db/migrate/20141106202902_application_accepted_default_to_false.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationAcceptedDefaultToFalse < ActiveRecord::Migration
-  def change
-    remove_column :applications, :accepted
-    remove_column :hackathons, :accepted
-    add_column :applications, :accepted, :boolean, :default => false
-    add_index :applications, :accepted
-  end
-end
