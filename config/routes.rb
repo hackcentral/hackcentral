f9e69bcaf03d5fc108ee1f352acbc8c2daeb112b
Jon Moss <me@jonathanmoss.me>
2014-11-16 15:00:44 -0500

    customized devise routing for signup and signin, renamed pwd modal

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   end
 
   # Devise
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 
   # Admin
   get "/admin" => "admin/dashboards#index"
