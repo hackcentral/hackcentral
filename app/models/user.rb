6bef10beb5aa8431922a993cf3c5a58b7240cd1d
Jon Moss <me@jonathanmoss.me>
2014-12-06 21:15:05 -0500

    Will be implementing the welcome email through a Devise confirm email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,4 @@ class User < ActiveRecord::Base
   has_many :submissions
   has_many :oauth_applications, class_name: 'Doorkeeper::Application', as: :owner
 
-  after_init :send_welcome_email
-
-private
-  def send_welcome_email
-    NotificationMailer.welcome_email(user).deliver_now
-  end
-
 end
