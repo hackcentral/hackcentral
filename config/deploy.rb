26f6d42b5e1cccb3782f43d65ae634b2c444533a
Jon Moss <me@jonathanmoss.me>
2014-12-10 20:24:54 -0500

    config/deploy.rb should be set

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,17 @@
 # config valid only for current version of Capistrano
 lock '3.3.4'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'hackcentral'
+set :scm, :git
+set :repo_url, 'git@github.com:me/hackcentral.git'
+set :deploy_to, '/home/deploy/hackcentral'
+set :rails_env, 'production'
+set :rbenv_type, :user
+set :rbenv_ruby, '2.1.3p242'
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
@@ -36,13 +45,13 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
 end
