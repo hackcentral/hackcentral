c8dedcc6bac55d3a386db470b4d8391c269b76d6
Jon Moss <me@jonathanmoss.me>
2014-12-12 18:00:44 -0500

    stuff

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -6,7 +6,7 @@ set -e
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deploy/hackcentral/current
-PID=$APP_ROOT/home/deploy/hackcentral/current/pids/unicorn.pid
+PID=$APP_ROOT/home/deploy/hackcentral/current/unicorn.pid
 CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 action="$1"
 set -u
