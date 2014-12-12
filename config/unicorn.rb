978b2a61ce3222740dfc3eda5f9907ebc3c83831
Jon Moss <me@jonathanmoss.me>
2014-12-12 17:55:03 -0500

    update hc path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,20 @@
 # Set the working application directory
 # working_directory "/path/to/your/app"
-working_directory "/home/deploy/hackcentral"
+working_directory "/home/deploy/hackcentral/current"
 
 # Unicorn PID file location
 # pid "/path/to/pids/unicorn.pid"
-pid "/home/deploy/hackcentral/pids/unicorn.pid"
+pid "/home/deploy/hackcentral/current/pids/unicorn.pid"
 
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/home/deploy/hackcentral/log/unicorn.log"
-stdout_path "/home/deploy/hackcentral/log/unicorn.log"
+stderr_path "/home/deploy/hackcentral/current/log/unicorn.log"
+stdout_path "/home/deploy/hackcentral/current/log/unicorn.log"
 
 # Unicorn socket
 listen "/tmp/unicorn.[app name].sock"
-listen "/tmp/unicorn.myapp.sock"
+listen "/tmp/unicorn.hackcentral.sock"
 
 # Number of processes
 # worker_processes 4
