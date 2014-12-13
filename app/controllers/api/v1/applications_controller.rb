e508c5bbc7a5b62cda7a83d02b01e334c87a2084
Jon Moss <me@jonathanmoss.me>
2014-12-13 15:22:00 -0500

    Created applications api controller

---
+++ app/controllers/api/v1/applications_controller.rb
@@ -0,0 +1,28 @@
+module Api
+  module V1
+    class ApplicationsController < ApplicationController
+      doorkeeper_for :all
+      respond_to :json
+
+      def index
+        respond_with current_user.applications
+      end
+
+      def show
+        respond_with current_user.application(params[:application])
+      end
+
+      def create
+        respond_with current_user.applications.create(params[:application])
+      end
+
+      def update
+        respond_with current_user.applications.update(params[:application])
+      end
+
+      def destroy
+        respond_with current_user.applications.destroy(params[:application])
+      end
+    end
+  end
+end
