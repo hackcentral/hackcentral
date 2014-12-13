6e81ffdba1339da97bf23204055d7c2d1944c815
Jon Moss <me@jonathanmoss.me>
2014-12-13 16:49:13 -0500

    Likes are completed! Fixes #8

---
+++ app/controllers/submissions/likes_controller.rb
@@ -0,0 +1,27 @@
+class Submissions::LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_submission
+
+  def create
+    @submission.likes.where(user_id: current_user.id).first_or_create
+
+    respond_to do |format|
+      format.html { redirect_to @submission }
+      format.js {}
+    end
+  end
+
+  def destroy
+    @submission.likes.where(user_id: current_user.id).destroy_all
+
+    respond_to do |format|
+      format.html { redirect_to @submission }
+      format.js {}
+    end
+  end
+
+  private
+    def set_submission
+      @submission = Submission.friendly.find(params[:submission_id])
+    end
+end
