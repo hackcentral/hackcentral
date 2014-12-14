21c33865a9f850a041d203cc0d43901b8c5852ba
Jon Moss <me@jonathanmoss.me>
2014-12-13 20:23:37 -0500

    generated profile model spec

---
+++ spec/models/profile_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe Profile do
+  it "has a valid factory"
+
+  it "belongs to a user"
+  it "has many applications"
+  
+  it "is invalid without a name"
+  it "is invalid without a first_name"
+  it "is invalid without a last_name"
+  it "is invalid without a email"
+  it "is invalid without a school_grad"
+  it "is invalid without a bio"
+end
