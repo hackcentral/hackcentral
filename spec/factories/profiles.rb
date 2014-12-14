f3b777f341099bad81afe1779b6f5198ce5b7b97
Jon Moss <me@jonathanmoss.me>
2014-12-13 21:21:13 -0500

    forgot to add a couple fields to profile model spec

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -10,6 +10,10 @@ FactoryGirl.define do
     f.email { Faker::Internet.email }
     f.school_grad { Faker::Number.number(4) }
     f.bio { Faker::Lorem.sentence }
+    f.website { Faker::Internet.domain_name }
+    f.github { Faker::Name.first_name }
+    f.dietary_needs { Faker::Lorem.sentence }
+    f.user_id { Faker::Number.number(4) }
   end
 
 end
