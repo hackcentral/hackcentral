f9b5f04c4b89c65b26d9b2e68f7eedc8ec443221
Jon Moss <me@jonathanmoss.me>
2014-12-13 19:34:12 -0500

    test: pages#contact

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -15,3 +15,11 @@ feature "About Page" do
     expect(page).to have_css 'h1', text: 'About'
   end
 end
+
+feature "Contact Page" do
+  scenario "succesfully" do
+    visit pages_contact_path
+
+    expect(page).to have_css 'h1', text: 'Contact'
+  end
+end
