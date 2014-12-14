d819427f0ebd9fd20dd823e49b9c1ba50abc24e3
Jon Moss <me@jonathanmoss.me>
2014-12-13 21:44:47 -0500

    Initialized ProfilesController spec

---
+++ spec/controllers/profiles_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe ProfilesController do
+  describe "GET #index" do
+  end
+  describe "GET #show" do
+  end
+  describe "GET #new" do
+  end
+  describe "POST #create" do
+  end
+  describe "GET #index" do
+  end
+  describe "GET #index" do
+  end
+end
