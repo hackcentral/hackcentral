faab2a1741252f21983ea7c9aac3be41103916e4
Jon Moss <me@jonathanmoss.me>
2014-12-13 20:46:08 -0500

    only 2 profile model tests left, need to test associations

---
+++ spec/factories/profiles.rb
@@ -0,0 +1,15 @@
+require 'faker'
+require 'factory_girl_rails'
+
+FactoryGirl.define do
+
+  factory :profile do |f|
+    f.name { Faker::Name.name }
+    f.first_name { Faker::Name.first_name }
+    f.last_name { Faker::Name.last_name }
+    f.email { Faker::Internet.email }
+    f.school_grad { Faker::Number.number(4) }
+    f.bio { Faker::Lorem.sentence }
+  end
+
+end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,15 +1,29 @@
 require "rails_helper"
 
 describe Profile do
-  it "has a valid factory"
+  it "has a valid factory" do
+    FactoryGirl.build(:profile).should be_valid
+  end
 
   it "belongs to a user"
   it "has many applications"
-  
-  it "is invalid without a name"
-  it "is invalid without a first_name"
-  it "is invalid without a last_name"
-  it "is invalid without a email"
-  it "is invalid without a school_grad"
-  it "is invalid without a bio"
+
+  it "is invalid without a name" do
+    FactoryGirl.build(:profile, :name => nil).should_not be_valid
+  end
+  it "is invalid without a first_name" do
+    FactoryGirl.build(:profile, :first_name => nil).should_not be_valid
+  end
+  it "is invalid without a last_name" do
+    FactoryGirl.build(:profile, :last_name => nil).should_not be_valid
+  end
+  it "is invalid without a email" do
+    FactoryGirl.build(:profile, :email => nil).should_not be_valid
+  end
+  it "is invalid without a school_grad" do
+    FactoryGirl.build(:profile, :school_grad => nil).should_not be_valid
+  end
+  it "is invalid without a bio" do
+    FactoryGirl.build(:profile, :bio => nil).should_not be_valid
+  end
 end
