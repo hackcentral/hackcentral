d29b553ee6343adeaa6f8b3d4c3c9b4f7a150590
Jon Moss <me@jonathanmoss.me>
2014-12-13 19:33:36 -0500

    test: pages#about

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -7,3 +7,11 @@ feature "Home Page" do
     expect(page).to have_css 'h1', text: 'Hack Central'
   end
 end
+
+feature "About Page" do
+  scenario "succesfully" do
+    visit pages_about_path
+
+    expect(page).to have_css 'h1', text: 'About'
+  end
+end
