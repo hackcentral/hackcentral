1ec7af52c90248de432a7cf29eeac28fa756ab2a
Jon Moss <me@jonathanmoss.me>
2014-12-15 20:13:12 -0500

    More ApplicationsController tests

--- spec/controllers/applications_controller_spec.rb
+++ spec/controllers/applications_controller_spec.rb
@@ -105,6 +105,17 @@ describe ApplicationsController, :type => :controller do
         put :update, id: @application, application: FactoryGirl.attributes_for(:application)
         assigns(:application).should eq(@application)
       end
+
+      it "does not change @application's attributes" do
+        put :update, id: @application, application: FactoryGirl.attributes_for(:application, reimbursement_needed: "a")
+        @application.reload
+        @application.reimbursement_needed.should_not eq("a")
+      end
+
+      #it "re-renders the edit method" do
+        #put :update, id: @application, application: FactoryGirl.attributes_for(:application, profile_id: nil)
+        #response.should render_template 'edit'
+      #end
     end
   end
 
