ed0e72f2dd0b13c003c4eb046054950b61599586
Jon Moss <me@jonathanmoss.me>
2014-12-15 20:33:30 -0500

    modified layout of Api::V1::UsersController

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
-describe Api::V1::UsersController, "#show" do
-  it 'returns a 401 when users are not authenticated' do
-    get :show
-    expect(response.code).to eq "401"
+describe Api::V1::UsersController do
+  context "no access token" do
+    it 'returns a 401 when users are not authenticated' do
+      get :show
+      expect(response.code).to eq "401"
+    end
   end
 end
