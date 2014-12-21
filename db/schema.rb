c8446c9fcba514d88fd844a40420710c3d67560d
Jon Moss <me@jonathanmoss.me>
2014-12-21 09:23:32 -0500

    DB migration for adding bio section to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141213210506) do
+ActiveRecord::Schema.define(version: 20141221142256) do
 
   create_table "applications", force: true do |t|
     t.boolean  "reimbursement_needed"
@@ -189,6 +189,7 @@ ActiveRecord::Schema.define(version: 20141213210506) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.text     "bio"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
