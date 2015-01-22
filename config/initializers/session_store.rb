3fb801796a17f2241f567e13c524214e3d0a33e9
Jon Moss <me@jonathanmoss.me>
2015-01-21 19:47:10 -0500

    simplify session storing

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_hackcentral_session', domain: {
-  production: '.hackcentral.co',
-  production: '.hackcentral-production.herokuapp.com',
-  development: '.vcap.me'
-}.fetch(Rails.env.to_sym, :all)
+Rails.application.config.session_store :cookie_store, key: '_hackcentral_session', domain: '.hackcentral.co'
+
+Rails.application.config.session_store :cookie_store, key: '_hackcentral_session', domain: '.vcap.me'
