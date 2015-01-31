a68046bf0f6e04595630a1d3f2cb7c6a2514620c
Jon Moss <me@jonathanmoss.me>
2015-01-31 12:50:13 -0500

    Applications entity

--- app/api/alpha/applications.rb
+++ app/api/alpha/applications.rb
@@ -1,4 +1,17 @@
 module Alpha
   class Applications < Grape::API
+
+    module Entities
+    class Application < Grape::Entity
+      expose :id
+      expose :reimbursement_needed, documentation: { type: "Boolean", desc: "If user needs travel reimbursement" }
+      expose :profile_id, documentation: { type: "Integer", desc: "ID of profile" }
+      expose :hackathon_id, documentation: { type: "Integer", desc: "ID of hackathon applying to" }
+      expose :created_at
+      expose :updated_at
+    end
+  end
+
+
   end
 end
