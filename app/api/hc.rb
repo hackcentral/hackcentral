a85ea5a2789f38eac9c062ba4a9572bee696e3ae
Jon Moss <me@jonathanmoss.me>
2015-02-08 08:47:07 -0500

    Tests!

--- app/api/hc.rb
+++ app/api/hc.rb
@@ -2,12 +2,19 @@ class HC < Grape::API
   format :json
   use WineBouncer::OAuth2
 
+  rescue_from WineBouncer::Errors::OAuthUnauthorizedError do |e|
+    Rack::Response.new({
+      error: "unauthorized_oauth",
+      error_description: "Please supply a valid access token."
+    }.to_json, 401).finish
+  end
+
   mount Alpha::Root
 
   desc 'This method uses Doorkeepers default scopes.',
     auth: { scopes: [] }
     get '/protected_with_default_scope' do
-       { hello: 'protected unscoped world' }
+      { hello: 'protected unscoped world' }
     end
 
   # error_formatter :json, API::ErrorFormatter
--- spec/api/hc_spec.rb
+++ spec/api/hc_spec.rb
@@ -2,6 +2,14 @@ require "rails_helper"
 
 RSpec.describe "hc api", :type => :request do
 
+  describe 'This method uses Doorkeepers default scopes.' do
+    it "renders 401" do
+      get 'https://api.vcap.me:3000/protected_with_default_scope'
+      response.status.should eq(401)
+      #expect(JSON.parse(response.body)).to eq [{ hello: 'protected unscoped world' }]
+    end
+  end
+
   describe "GET /applications" do
     it "returns 401" do
       get "https://api.vcap.me:3000/v1/applications"
