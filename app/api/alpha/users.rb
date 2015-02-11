ff4f6ed2cf7fd808fc2914b7d6b0b0e71a605f70
Jon Moss <me@jonathanmoss.me>
2015-02-11 16:02:34 -0500

    users#show functionality

--- app/api/alpha/users.rb
+++ app/api/alpha/users.rb
@@ -20,5 +20,13 @@ module Alpha
         error_description: "Please supply a valid access token."
       }.to_json, 401).finish
     end
+
+    desc "Show current_user's information (Doorkeeper Auth)", auth: { scopes: [] }, entity: Alpha::Entities::User
+      get '/user', http_codes: [ [200, "Ok", Alpha::Entities::User] ] do
+        @user = User.find_by_id(resource_owner.id)
+
+        status 200
+        present @user, with: Alpha::Entities::User
+      end
   end
 end
