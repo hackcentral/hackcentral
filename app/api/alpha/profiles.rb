0e4777142a7a1a82eccf94d3043dd67e9cca0721
Jon Moss <me@jonathanmoss.me>
2015-02-13 17:28:02 -0500

    Profile Grape Entity

--- app/api/alpha/profiles.rb
+++ app/api/alpha/profiles.rb
@@ -2,6 +2,16 @@ module Alpha
 
   module Entities
     class Profile < Grape::Entity
+      expose :id
+      expose :name, documentation: { type: "String", desc: "Name" }
+      expose :school_grad, documentation: { type: "String", desc: "Year of graduation" }
+      expose :website, documentation: { type: "String", desc: "Website" }
+      expose :github, documentation: { type: "String", desc: "GitHub" }
+      expose :created_at
+      expose :updated_at
+      expose :resume_url
+      expose :dietary_needs, documentation: { type: "String", desc: "Dietary needs" }
+      expose :user_id
     end
   end
 
