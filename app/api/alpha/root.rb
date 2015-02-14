ce5fb5aec996d04fca8800e533db892a818fd0f2
Jon Moss <me@jonathanmoss.me>
2015-02-13 22:05:40 -0500

    going to start over with Alpha::Profiles

--- app/api/alpha/root.rb
+++ app/api/alpha/root.rb
@@ -6,7 +6,6 @@ module Alpha
     # Mountings
     mount Alpha::Applications
     mount Alpha::Hackathons
-    mount Alpha::Profiles
     mount Alpha::Users
 
     #desc "Returns pong."
@@ -14,9 +13,9 @@ module Alpha
       #{ ping: "pong" }
     #end
 
-    route :any, '*path' do
-      error! # or something else
-    end
+    #route :any, '*path' do
+      #error! # or something else
+    #end
 
   end
 end
