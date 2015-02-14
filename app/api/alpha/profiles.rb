0b338d3cc76ee8c164a84b7c69403a8a2af60277
Jon Moss <me@jonathanmoss.me>
2015-02-14 10:22:29 -0500

    GET#index profiles

--- app/api/alpha/profiles.rb
+++ app/api/alpha/profiles.rb
@@ -15,5 +15,20 @@ module Alpha
   end
 
   class Profiles < Grape::API
+    format :json
+    use WineBouncer::OAuth2
+
+    rescue_from WineBouncer::Errors::OAuthUnauthorizedError do |e|
+      Rack::Response.new({
+        error: "unauthorized_oauth",
+        error_description: "Please supply a valid access token."
+      }.to_json, 401).finish
+    end
+
+    desc "Show all profiles (Doorkeeper Auth)", auth: { scopes: [] }, entity: Alpha::Entities::Profile
+      get '/profiles', http_codes: [ [200, "Ok", Alpha::Entities::Profile] ] do
+        profiles = Profile.where(user_id: resource_owner).all
+        present profiles, with: Alpha::Entities::Profile
+      end
   end
 end
--- spec/api/alpha/profiles_spec.rb
+++ spec/api/alpha/profiles_spec.rb
@@ -4,8 +4,21 @@ RSpec.describe "Alpha::Profiles", :type => :request do
   let!(:user) { create(:user) }
 
   context "no access token" do
+    it 'returns a 401 when users are not authenticated' do
+      get "https://api.vcap.me:3000/v1/profiles"
+      response.status.should eq(401)
+    end
   end
 
   context "with access token" do
+    describe 'GET #index' do
+      it "should show all of user's applications" do
+        @oauth_application = FactoryGirl.build(:oauth_application)
+        @token = Doorkeeper::AccessToken.create!(:application_id => @oauth_application.id, :resource_owner_id => user.id)
+
+        get "https://api.vcap.me:3000/v1/profiles", :format => :json, :access_token => @token.token
+        response.status.should eq(200)
+      end
+    end
   end
 end
