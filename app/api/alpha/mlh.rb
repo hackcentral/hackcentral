cd94749a771e9b5c152e1747f3011c5d957bd6c1
Jon Moss <me@jonathanmoss.me>
2015-02-15 11:09:06 -0500

    PUT#update unsanctioning Alpha::MLH

--- app/api/alpha/mlh.rb
+++ app/api/alpha/mlh.rb
@@ -14,7 +14,6 @@ module Alpha
     desc "Sanction a hackathon (Doorkeeper Auth)", auth: { scopes: [] }, entity: Alpha::Entities::Hackathon
       params do
         requires :hackathon_id, type: Integer, desc: "ID of hackathon that is being sanctioned"
-        #requires :mlh_sanctioned, type: String, desc: "mlh_sanctioned is true or false?"
       end
 
       put '/mlh/sanction/:hackathon_id', http_codes: [ [200, "Ok", Alpha::Entities::Hackathon] ] do
@@ -34,5 +33,26 @@ module Alpha
         end
       end
 
+    desc "Unsanction a hackathon (Doorkeeper Auth)", auth: { scopes: [] }, entity: Alpha::Entities::Hackathon
+      params do
+        requires :hackathon_id, type: Integer, desc: "ID of hackathon that is being sanctioned"
+      end
+
+      put '/mlh/unsanction/:hackathon_id', http_codes: [ [200, "Ok", Alpha::Entities::Hackathon] ] do
+        @hackathon = Hackathon.find(params[:hackathon_id])
+
+        if resource_owner.mlh?
+          @hackathon.mlh_sanctioned = false
+          @hackathon.save
+
+          status 200
+          present @hackathon, with: Alpha::Entities::Hackathon
+        else
+          Rack::Response.new({
+            error: "unauthorized_oauth",
+            error_description: "Please supply a valid access token."
+          }.to_json, 401).finish
+        end
+      end
   end
 end
--- spec/api/alpha/mlh_spec.rb
+++ spec/api/alpha/mlh_spec.rb
@@ -50,5 +50,42 @@ RSpec.describe "Alpha::MLH", :type => :request do
       end
     end
 
+    describe 'PUT #update --> UNSANCTION' do
+      before :each do
+        @oauth_application = FactoryGirl.build(:oauth_application)
+        @token = Doorkeeper::AccessToken.create!(:application_id => @oauth_application.id, :resource_owner_id => user.id)
+        @hackathon = FactoryGirl.create(:hackathon)
+      end
+
+      context "valid attributes && user == mlh" do
+        it "sanction the @hackathon" do
+          put "http://api.vcap.me:3000/v1/mlh/unsanction/1?access_token=#{@token.token}", :format => :json
+          response.status.should eq(200)
+        end
+
+        it "changes @hackathon's attributes" do
+          put "http://api.vcap.me:3000/v1/mlh/unsanction/1?access_token=#{@token.token}", :format => :json
+          @hackathon.reload
+          @hackathon.mlh_sanctioned.should eq(false)
+        end
+
+        it "sends a 200 if updated hackathon if user == mlh" do
+          put "http://api.vcap.me:3000/v1/mlh/unsanction/1?access_token=#{@token.token}", :format => :json
+          response.status.should eq(200)
+        end
+      end
+
+      context "valid attributes && user != mlh" do
+        it "returns 401" do
+          put "http://api.vcap.me:3000/v1/mlh/unsanction/1?access_token=#{@token.token}", :format => :json
+          if user.mlh == true
+            response.status.should eq(200)
+          else
+            response.status.should eq(401)
+          end
+        end
+      end
+    end
+
   end
 end
