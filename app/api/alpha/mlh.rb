13b6ce1d54706407ad182b510e5b3f17caa14018
Jon Moss <me@jonathanmoss.me>
2015-02-14 21:45:39 -0500

    PUT#update sanctioning Alpha::MLH

--- app/api/alpha/mlh.rb
+++ app/api/alpha/mlh.rb
@@ -1,5 +1,38 @@
 module Alpha
 
   class MLH < Grape::API
+    format :json
+    use WineBouncer::OAuth2
+
+    rescue_from WineBouncer::Errors::OAuthUnauthorizedError do |e|
+      Rack::Response.new({
+        error: "unauthorized_oauth",
+        error_description: "Please supply a valid access token."
+      }.to_json, 401).finish
+    end
+
+    desc "Sanction a hackathon (Doorkeeper Auth)", auth: { scopes: [] }, entity: Alpha::Entities::Hackathon
+      params do
+        requires :hackathon_id, type: Integer, desc: "ID of hackathon that is being sanctioned"
+        #requires :mlh_sanctioned, type: String, desc: "mlh_sanctioned is true or false?"
+      end
+
+      put '/mlh/sanction/:hackathon_id', http_codes: [ [200, "Ok", Alpha::Entities::Hackathon] ] do
+        @hackathon = Hackathon.find(params[:hackathon_id])
+
+        if resource_owner.mlh?
+          @hackathon.mlh_sanctioned = true
+          @hackathon.save
+
+          status 200
+          present @hackathon, with: Alpha::Entities::Hackathon
+        else
+          Rack::Response.new({
+            error: "unauthorized_oauth",
+            error_description: "Please supply a valid access token."
+          }.to_json, 401).finish
+        end
+      end
+
   end
 end
---
+++ spec/api/alpha/mlh_spec.rb
@@ -0,0 +1,54 @@
+require "rails_helper"
+
+RSpec.describe "Alpha::MLH", :type => :request do
+
+  let!(:user) { create(:user, mlh: true) }
+
+  context "no access token" do
+    it 'returns a 401 when users are not authenticated' do
+      put "https://api.vcap.me:3000/v1/mlh/sanction/1"
+      response.status.should eq(401)
+    end
+  end
+
+  context "with access token" do
+
+    describe 'PUT #update --> SANCTION' do
+      before :each do
+        @oauth_application = FactoryGirl.build(:oauth_application)
+        @token = Doorkeeper::AccessToken.create!(:application_id => @oauth_application.id, :resource_owner_id => user.id)
+        @hackathon = FactoryGirl.create(:hackathon)
+      end
+
+      context "valid attributes && user == mlh" do
+        it "sanction the @hackathon" do
+          put "http://api.vcap.me:3000/v1/mlh/sanction/1?access_token=#{@token.token}", :format => :json
+          response.status.should eq(200)
+        end
+
+        it "changes @hackathon's attributes" do
+          put "http://api.vcap.me:3000/v1/mlh/sanction/1?access_token=#{@token.token}", :format => :json
+          @hackathon.reload
+          @hackathon.mlh_sanctioned.should eq(true)
+        end
+
+        it "sends a 200 if updated hackathon if user == mlh" do
+          put "http://api.vcap.me:3000/v1/mlh/sanction/1?access_token=#{@token.token}", :format => :json
+          response.status.should eq(200)
+        end
+      end
+
+      context "valid attributes && user != mlh" do
+        it "returns 401" do
+          put "http://api.vcap.me:3000/v1/mlh/sanction/1?access_token=#{@token.token}", :format => :json
+          if user.mlh == true
+            response.status.should eq(200)
+          else
+            response.status.should eq(401)
+          end
+        end
+      end
+    end
+
+  end
+end
