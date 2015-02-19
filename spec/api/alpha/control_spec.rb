607e54aab5a138b71c6d584e72e844fdc1004c76
Jon Moss <me@jonathanmoss.me>
2015-02-19 17:16:00 -0500

    Alpha::Control view application to hackathon

--- spec/api/alpha/control_spec.rb
+++ spec/api/alpha/control_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe "Alpha::Control", :type => :request do
       get "https://api.vcap.me:3000/v1/hackathons/1/applications"
       response.status.should eq(401)
     end
+    it 'returns a 401 when users are not authenticated' do
+      get "https://api.vcap.me:3000/v1/hackathons/1/applications/1"
+      response.status.should eq(401)
+    end
   end
 
   context "with access token" do
@@ -36,6 +40,38 @@ RSpec.describe "Alpha::Control", :type => :request do
       it "if organizer?" do
         if user.organizers.where(hackathon_id: @hackathon)
           get "https://api.vcap.me:3000/v1/hackathons/1/applications", :format => :json, :access_token => @token.token
+          response.status.should eq(200)
+        else
+          response.status.should eq(401)
+        end
+      end
+    end
+
+    describe 'GET applications#show' do
+      before :each do
+        @oauth_application = FactoryGirl.build(:oauth_application)
+        @token = Doorkeeper::AccessToken.create!(:application_id => @oauth_application.id, :resource_owner_id => user.id)
+        @hackathon = FactoryGirl.create(:hackathon, user_id: '1')
+        @application = FactoryGirl.create(:application, hackathon_id: '1')
+      end
+
+      it "should show application to hackathon" do
+        get "https://api.vcap.me:3000/v1/hackathons/1/applications/1", :format => :json, :access_token => @token.token
+        response.status.should eq(200)
+      end
+
+      it "if user_id ==?" do
+        if @hackathon.user_id == user.id
+          get "https://api.vcap.me:3000/v1/hackathons/1/applications/1", :format => :json, :access_token => @token.token
+          response.status.should eq(200)
+        else
+          response.status.should eq(401)
+        end
+      end
+
+      it "if organizer?" do
+        if user.organizers.where(hackathon_id: @hackathon)
+          get "https://api.vcap.me:3000/v1/hackathons/1/applications/1", :format => :json, :access_token => @token.token
           response.status.should eq(200)
         else
           response.status.should eq(401)
