6ef3a4a4d6f2bdb54959129da7e8440fb7e17ef6
Jon Moss <me@jonathanmoss.me>
2015-02-19 17:11:09 -0500

    Alpha::Control view applications for hackathon

--- app/api/alpha/control.rb
+++ app/api/alpha/control.rb
@@ -1,5 +1,35 @@
 module Alpha
 
   class Control < Grape::API
+    format :json
+    use WineBouncer::OAuth2
+
+    rescue_from WineBouncer::Errors::OAuthUnauthorizedError do |e|
+      Rack::Response.new({
+        error: "unauthorized_oauth",
+        error_description: "Please supply a valid access token."
+      }.to_json, 401).finish
+    end
+
+    desc "Show all applications to hackathon (Doorkeeper Auth)", auth: { scopes: [] }, entity: Alpha::Entities::Application
+      params do
+        requires :hackathon_id, type: Integer, desc: "ID of hackathon"
+      end
+
+
+      get '/hackathons/:hackathon_id/applications', http_codes: [ [200, "Ok", Alpha::Entities::Application] ] do
+        @hackathon = Hackathon.find(params[:hackathon_id])
+
+        if @hackathon.user_id == resource_owner.id or resource_owner.organizers.where(hackathon_id: @hackathon)
+          @applications = Application.where(hackathon_id: @hackathon)
+          status 200
+          present @applications, with: Alpha::Entities::Application
+        else
+          Rack::Response.new({
+            error: "unauthorized_oauth",
+            error_description: "Please supply a valid access token."
+          }.to_json, 401).finish
+        end
+      end
   end
 end
--- spec/api/alpha/control_spec.rb
+++ spec/api/alpha/control_spec.rb
@@ -5,8 +5,42 @@ RSpec.describe "Alpha::Control", :type => :request do
   let!(:user) { create(:user, mlh: true) }
 
   context "no access token" do
+    it 'returns a 401 when users are not authenticated' do
+      get "https://api.vcap.me:3000/v1/hackathons/1/applications"
+      response.status.should eq(401)
+    end
   end
 
   context "with access token" do
+    describe 'GET applications#index' do
+      before :each do
+        @oauth_application = FactoryGirl.build(:oauth_application)
+        @token = Doorkeeper::AccessToken.create!(:application_id => @oauth_application.id, :resource_owner_id => user.id)
+        @hackathon = FactoryGirl.create(:hackathon, user_id: '1')
+      end
+
+      it "should show all applications to hackathon" do
+        get "https://api.vcap.me:3000/v1/hackathons/1/applications", :format => :json, :access_token => @token.token
+        response.status.should eq(200)
+      end
+
+      it "if user_id ==?" do
+        if @hackathon.user_id == user.id
+          get "https://api.vcap.me:3000/v1/hackathons/1/applications", :format => :json, :access_token => @token.token
+          response.status.should eq(200)
+        else
+          response.status.should eq(401)
+        end
+      end
+
+      it "if organizer?" do
+        if user.organizers.where(hackathon_id: @hackathon)
+          get "https://api.vcap.me:3000/v1/hackathons/1/applications", :format => :json, :access_token => @token.token
+          response.status.should eq(200)
+        else
+          response.status.should eq(401)
+        end
+      end
+    end
   end
 end
