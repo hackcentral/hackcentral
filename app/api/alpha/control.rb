65565cbf50d14815c2f7a875c6961c60c4829c19
Jon Moss <me@jonathanmoss.me>
2015-02-20 12:39:25 -0500

    Alpha::Control, update a hackathon

--- app/api/alpha/control.rb
+++ app/api/alpha/control.rb
@@ -1,5 +1,55 @@
 module Alpha
 
   class Control < Grape::API
+    format :json
+    use WineBouncer::OAuth2
+
+    rescue_from WineBouncer::Errors::OAuthUnauthorizedError do |e|
+      Rack::Response.new({
+        error: "unauthorized_oauth",
+        error_description: "Please supply a valid access token."
+      }.to_json, 401).finish
+    end
+
+    desc "Update a hackathon (Doorkeeper Auth)", auth: { scopes: [] }, entity: Alpha::Entities::Hackathon
+      params do
+        optional :name, type: String, desc: "Name of hackathon"
+        optional :about, type: String, desc: "About of hackathon"
+        optional :tagline, type: String, desc: "Tagline of hackathon"
+        optional :location, type: String, desc: "Location of hackathon"
+        optional :start, type: String, desc: "Start time of hackathon"
+        optional :end, type: String, desc: "End time of hackathon"
+        optional :logo, desc: "Logo image of hackathon"
+        optional :header, desc: "Header image of hackathon"
+        optional :hs_hackers_allowed, type: String, desc: "If hs_hackers_allowed at hackathon"
+        optional :subdomain, type: String, desc: "Subdomain of hackathon"
+      end
+
+      put '/hackathons/:id', http_codes: [ [200, "Ok", Alpha::Entities::Hackathon] ] do
+        @hackathon = Hackathon.find(params[:id])
+
+        if @hackathon.user_id == resource_owner.id or resource_owner.organizers.where(hackathon_id: @hackathon)
+
+          @hackathon.name = params[:name] if params[:name]
+          @hackathon.about = params[:about] if params[:about]
+          @hackathon.tagline = params[:tagline] if params[:tagline]
+          @hackathon.location = params[:location] if params[:location]
+          @hackathon.start = params[:start] if params[:start]
+          @hackathon.end = params[:end] if params[:end]
+          @hackathon.logo = params[:logo] if params[:logo]
+          @hackathon.header = params[:header] if params[:header]
+          @hackathon.hs_hackers_allowed = params[:hs_hackers_allowed] if params[:hs_hackers_allowed]
+          @hackathon.subdomain = params[:subdomain] if params[:subdomain]
+          @hackathon.save
+
+          status 200
+          present @hackathon, with: Alpha::Entities::Hackathon
+        else
+          Rack::Response.new({
+            error: "unauthorized_oauth",
+            error_description: "Please supply a valid access token."
+          }.to_json, 401).finish
+        end
+      end
   end
 end
--- spec/api/alpha/control_spec.rb
+++ spec/api/alpha/control_spec.rb
@@ -1,10 +1,71 @@
 require "rails_helper"
 
-RSpec.describe "Alpha::Organizer", :type => :request do
+RSpec.describe "Alpha::Control", :type => :request do
+
+  let!(:user) { create(:user) }
 
   context "no access token" do
+    it 'returns a 401 when users are not authenticated' do
+      put "https://api.vcap.me:3000/v1/hackathons/1"
+      response.status.should eq(401)
+    end
   end
 
   context "with access token" do
+    describe 'PUT #update' do
+      before :each do
+        @oauth_application = FactoryGirl.build(:oauth_application)
+        @token = Doorkeeper::AccessToken.create!(:application_id => @oauth_application.id, :resource_owner_id => user.id)
+        @hackathon = FactoryGirl.create(:hackathon, user_id: '1')
+      end
+
+      context "valid attributes && user_id ==?" do
+        it "locate @hackathon" do
+          put "https://api.vcap.me:3000/v1/hackathons/1?access_token=#{@token.token}", FactoryGirl.attributes_for(:hackathon, name: "test"), :format => :json
+          response.status.should eq(200)
+        end
+
+        it "changes @hackathon's attributes" do
+          put "https://api.vcap.me:3000/v1/hackathons/1?access_token=#{@token.token}", FactoryGirl.attributes_for(:hackathon, name: "test"), :format => :json
+          @hackathon.reload
+          @hackathon.name.should eq("test")
+        end
+      end
+
+      context "valid attributes && user_id !=" do
+        it "returns 401" do
+          put "https://api.vcap.me:3000/v1/hackathons/1?access_token=#{@token.token}", FactoryGirl.attributes_for(:hackathon, name: "test"), :format => :json
+          if @hackathon.user_id == user.id
+            response.status.should eq(200)
+          else
+            response.status.should eq(401)
+          end
+        end
+      end
+
+      context "valid attributes && organizer?" do
+        it "sanction the @hackathon" do
+          put "https://api.vcap.me:3000/v1/hackathons/1?access_token=#{@token.token}", FactoryGirl.attributes_for(:hackathon), :format => :json
+          response.status.should eq(200)
+        end
+
+        it "changes @hackathon's attributes" do
+          put "https://api.vcap.me:3000/v1/hackathons/1?access_token=#{@token.token}", FactoryGirl.attributes_for(:hackathon, name: "test"), :format => :json
+          @hackathon.reload
+          @hackathon.name.should eq("test")
+        end
+      end
+
+      context "valid attributes && organizer !=" do
+        it "returns 401" do
+          put "https://api.vcap.me:3000/v1/hackathons/1?access_token=#{@token.token}", FactoryGirl.attributes_for(:hackathon, name: "test"), :format => :json
+          if user.organizers.where(hackathon_id: @hackathon)
+            response.status.should eq(200)
+          else
+            response.status.should eq(401)
+          end
+        end
+      end
+    end
   end
 end
