13e279dc0b97df23dc04774b6ceb9d536ac33353
Jon Moss <me@jonathanmoss.me>
2015-02-22 09:14:53 -0500

    Admin::Organizers create

--- spec/controllers/admin/organizers_controller_spec.rb
+++ spec/controllers/admin/organizers_controller_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Admin::OrganizersController, :type => :controller do
 
   login_user
 
+  let!(:user) { create(:user, mlh: true) }
+
   describe "GET #index" do
     before :each do
       @hackathon = FactoryGirl.create(:hackathon)
@@ -25,6 +27,85 @@ RSpec.describe Admin::OrganizersController, :type => :controller do
       get :new, hackathon_id: @hackathon
       response.should render_template 'new'
       response.status.should eq(200)
+    end
+  end
+
+  describe "POST #create" do
+    before :each do
+      @hackathon = FactoryGirl.create(:hackathon)
+      @hackathon1 = FactoryGirl.create(:hackathon, user_id: "2")
+    end
+
+    context "valid attributes && user_id ==" do
+      it "creates a new organizer" do
+        if @hackathon.user_id == @current_user
+          expect{
+            post :create, hackathon_id: @hackathon, organizer: FactoryGirl.attributes_for(:organizer)
+          } .to change(Organizer, :count).by(1)
+        end
+      end
+
+      it "redirects to admin_hackathon_organizers_path" do
+        if @hackathon.user_id == @current_user
+          post :create, hackathon_id: @hackathon, organizer: FactoryGirl.attributes_for(:organizer)
+          response.should redirect_to admin_hackathon_organizers_path
+        end
+      end
+    end
+
+    context "valid attributes && organizer ==" do
+      it "creates a new organizer" do
+        if user.organizers.where(hackathon_id: @hackathon)
+          expect{
+            post :create, hackathon_id: @hackathon, organizer: FactoryGirl.attributes_for(:organizer)
+          } .to change(Organizer, :count).by(1)
+        end
+      end
+
+      it "redirects to admin_hackathon_organizers_path" do
+        if user.organizers.where(hackathon_id: @hackathon)
+          post :create, hackathon_id: @hackathon, organizer: FactoryGirl.attributes_for(:organizer)
+          response.should redirect_to admin_hackathon_organizers_path
+        end
+      end
+    end
+
+    context "valid attributes && user_id !=" do
+      it "will not create a new organizer" do
+        if @hackathon1.user_id == @current_user
+          expect{
+            post :create, hackathon_id: @hackathon1, organizer: FactoryGirl.attributes_for(:organizer)
+          } .to change(Organizer, :count).by(0)
+        end
+      end
+
+      it "redirects to root_path" do
+        if @hackathon1.user_id == @current_user
+          post :create, hackathon_id: @hackathon1, organizer: FactoryGirl.attributes_for(:organizer)
+          response.should redirect_to root_path
+          flash[:notice].should == "Not authorized"
+        end
+      end
+    end
+
+    context "valid attributes && organizer !=" do
+      it "will not create a new organizer" do
+        if user.organizers.where(hackathon_id: @hackathon1)
+        else
+          expect{
+            post :create, hackathon_id: @hackathon1, organizer: FactoryGirl.attributes_for(:organizer)
+          } .to change(Organizer, :count).by(0)
+        end
+      end
+
+      it "redirects to root_path" do
+        if user.organizers.where(hackathon_id: @hackathon)
+        else
+          post :create, hackathon_id: @hackathon1, organizer: FactoryGirl.attributes_for(:organizer)
+          response.should redirect_to root_path
+          flash[:notice].should == "Not authorized"
+        end
+      end
     end
   end
 end
