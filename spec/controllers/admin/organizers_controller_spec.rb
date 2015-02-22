f73a0a445ee71efe4cfe48003274b04976b43c30
Jon Moss <me@jonathanmoss.me>
2015-02-22 10:55:44 -0500

    Admin::Organizers delete

--- spec/controllers/admin/organizers_controller_spec.rb
+++ spec/controllers/admin/organizers_controller_spec.rb
@@ -108,4 +108,86 @@ RSpec.describe Admin::OrganizersController, :type => :controller do
       end
     end
   end
+
+  describe "DELETE #destroy" do
+    before :each do
+      @hackathon = FactoryGirl.create(:hackathon)
+      @organizer = FactoryGirl.create(:organizer)
+      @hackathon1 = FactoryGirl.create(:hackathon, user_id: '3')
+    end
+
+    context "valid attributes && correct_user" do
+      it "deletes the organizer" do
+        if @hackathon.user_id == @current_user
+          expect{
+            delete :destroy, hackathon_id: @hackathon, id: @organizer
+          }.to change(Organizer,:count).by(-1)
+        end
+      end
+
+      it "redirects to admin_hackathon_path" do
+        if @hackathon.user_id == @current_user
+          delete :destroy, hackathon_id: @hackathon, id: @organizer
+          response.should redirect_to admin_hackathon_path
+        end
+      end
+    end
+
+    context "valid attributes && organizer" do
+      it "deletes the hackathon" do
+        if user.organizers.where(hackathon_id: @hackathon)
+          expect{
+            delete :destroy, hackathon_id: @hackathon, id: @organizer
+          }.to change(Organizer,:count).by(-1)
+        end
+      end
+
+      it "redirects to admin_hackathon_path" do
+        if user.organizers.where(hackathon_id: @hackathon)
+          delete :destroy, hackathon_id: @hackathon, id: @organizer
+          response.should redirect_to admin_hackathon_path
+        end
+      end
+    end
+
+    context "valid attributes && correct_user !=" do
+      it "will not delete hackathon" do
+        if @hackathon1.user_id == @current_user
+        else
+          expect{
+            delete :destroy, hackathon_id: @hackathon1, id: @organizer
+          }.to change(Organizer,:count).by(0)
+        end
+      end
+
+      it "redirects to root_path" do
+        if @hackathon1.user_id == @current_user
+        else
+          delete :destroy, hackathon_id: @hackathon1, id: @organizer
+          response.should redirect_to root_path
+          flash[:notice].should == "Not authorized"
+        end
+      end
+    end
+
+    context "valid attributes && organizer !=" do
+      it "will not delete hackathon" do
+        if user.organizers.where(hackathon_id: @hackathon1)
+        else
+          expect{
+            delete :destroy, hackathon_id: @hackathon1, id: @organizer
+          }.to change(Organizer,:count).by(0)
+        end
+      end
+
+      it "redirects to root_path" do
+        if user.organizers.where(hackathon_id: @hackathon1)
+        else
+          delete :destroy, hackathon_id: @hackathon1, id: @organizer
+          response.should redirect_to root_path
+          flash[:notice].should == "Not authorized"
+        end
+      end
+    end
+  end
 end
