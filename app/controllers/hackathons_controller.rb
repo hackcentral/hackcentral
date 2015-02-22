a0376576e700f7920d24bdcc1a1dabc9b12cde99
Jon Moss <me@jonathanmoss.me>
2015-02-22 17:07:40 -0500

    HackathonsController cruft

--- app/controllers/hackathons_controller.rb
+++ app/controllers/hackathons_controller.rb
@@ -1,6 +1,4 @@
 class HackathonsController < ApplicationController
-#  before_action :set_hackathon, only: [:destroy, :edit, :update]
-  before_action :is_organizer, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:show]
   #before_action :current_hackathon, only: [:show]
 
@@ -21,10 +19,6 @@ class HackathonsController < ApplicationController
     @hackathon = current_user.hackathons.build #Hackathon.new
   end
 
-  # GET /hackathons/1/edit
-#  def edit
-#  end
-
   # POST /hackathons
   # POST /hackathons.json
   def create
@@ -39,48 +33,9 @@ class HackathonsController < ApplicationController
     end
   end
 
-#  # PATCH/PUT /hackathons/1
-#  # PATCH/PUT /hackathons/1.json
-#  def update
-#    respond_to do |format|
-#      if @hackathon.update(hackathon_params)
-#        format.html { redirect_to hackathons_path, notice: 'Hackathon was successfully updated.' }
-#      else
-#        format.html { render :edit }
-#      end
-#    end
-#  end
-
-  # DELETE /hackathons/1
-  # DELETE /hackathons/1.json
-#  def destroy
-#    @hackathon.destroy
-#    respond_to do |format|
-#      format.html { redirect_to hackathons_url, notice: 'Hackathon was successfully destroyed.' }
-#    end
-#  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-#    def set_hackathon
-#      @hackathon = Hackathon.find(params[:id])
-#    end
-
     def current_hackathon
       @current_hackathon ||= Hackathon.find_by_subdomain!(request.subdomain)
-    end
-
-    def is_organizer
-      if user_signed_in?
-        if @hackathon = current_user.hackathons.find_by(id: params[:id])
-          else redirect_to root_path, notice: "Not authorized" if @hackathon.nil?
-        end
-
-        if current_user.organizers.where(hackathon_id: @hackathon)
-          else redirect_to root_path, notice: "Not authorized" if @organizer.nil?
-        end
-      else
-      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
