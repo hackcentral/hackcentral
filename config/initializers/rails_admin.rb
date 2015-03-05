5082c98f6e2c7b76c42a87f6a2b151b5d707dfcb
Jon Moss <me@jonathanmoss.me>
2015-03-05 08:56:15 -0500

    working on rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,18 +1,16 @@
 RailsAdmin.config do |config|
 
-  ### Popular gems integration
-
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
-  ## == Cancan ==
-  # config.authorize_with :cancan
+  config.authorize_with do
+    redirect_to "/", :alert => "You are not authorized to access that page" unless current_user.admin?
+  end
 
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+  config.main_app_name = ['HackCentral', 'Admin']
 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
