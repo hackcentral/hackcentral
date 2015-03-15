54c52e43dcefa37677da403a242ef458f0e3b652
Jon Moss <me@jonathanmoss.me>
2015-03-15 11:57:15 -0400

    submission entity

--- app/api/alpha/submissions.rb
+++ app/api/alpha/submissions.rb
@@ -1,6 +1,18 @@
 module Alpha
   module Entities
     class Submission < Grape::Entity
+      expose :id
+      expose :title
+      expose :tagline
+      expose :description
+      expose :video
+      expose :website
+      expose :created_at
+      expose :updated_at
+      expose :user_id
+      expose :hackathon_id
+      expose :submitted_at
+      expose :slug
     end
   end
 
